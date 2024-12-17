dbe579b5b18010efb03c4454a825a9964765b0ed
Chris McGroarty <iammarix@gmail.com>
2024-12-17 11:17:48 -0500

    add powersettings script

---
+++ src/scripts/Optimize-PowerSettings.ps1
@@ -0,0 +1,9 @@
+Powercfg /Change monitor-timeout-ac 5
+Powercfg /Change disk-timeout-ac 5
+Powercfg /Change standby-timeout-ac 0
+Powercfg /Change hibernate-timeout-ac 0
+
+Powercfg /Change monitor-timeout-dc 2
+Powercfg /Change disk-timeout-dc 2
+Powercfg /Change standby-timeout-dc 10
+Powercfg /Change hibernate-timeout-dc 180
